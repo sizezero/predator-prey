f3a8d4df281446a064f7e703fa7aa6837bcd8282
Robert Kleemann <robert@kleemann.org>
2014-11-03 18:20:01 -0800

    add expandable component for map placeholder

--- src/main/scala/org/kleemann/predprey/Main.scala
+++ src/main/scala/org/kleemann/predprey/Main.scala
@@ -5,6 +5,16 @@ import swing._
 object Main extends SimpleSwingApplication {
   def top = new MainFrame {
     title = "ScalaSheet"
-    contents = new Label("TODO")
+    val l1 = new Label("TODO: CONTROL")
+    val l2 = new Component {
+      border = Swing.LineBorder(java.awt.Color.BLACK)
+      // TODO: example code has this without Dimension object "preferredSize = (300,200)"
+      preferredSize = new java.awt.Dimension(300, 200)
+    }
+    contents = new BoxPanel(Orientation.Horizontal ) {
+      contents.append(l1, l2)
+      border = Swing.EmptyBorder(5, 5, 5, 5)
+    }
   }
+  
 }
