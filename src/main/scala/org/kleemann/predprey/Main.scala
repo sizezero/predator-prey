bdc92187f834dd6c0dd5705479f480287357024d
Robert Kleemann <robert@kleemann.org>
2014-11-03 18:31:09 -0800

    make custom component for map

--- src/main/scala/org/kleemann/predprey/Main.scala
+++ src/main/scala/org/kleemann/predprey/Main.scala
@@ -1,16 +1,14 @@
 package org.kleemann.predprey
 
-import swing._
+import scala.swing._
+// TODO: calling this package swing makes the other imports a bit wordier
+import org.kleemann.predprey.swing._
 
 object Main extends SimpleSwingApplication {
   def top = new MainFrame {
-    title = "ScalaSheet"
+    title = "Predator / Prey"
     val l1 = new Label("TODO: CONTROL")
-    val l2 = new Component {
-      border = Swing.LineBorder(java.awt.Color.BLACK)
-      // TODO: example code has this without Dimension object "preferredSize = (300,200)"
-      preferredSize = new java.awt.Dimension(300, 200)
-    }
+    val l2 = new swing.MapComponent
     contents = new BoxPanel(Orientation.Horizontal ) {
       contents.append(l1, l2)
       border = Swing.EmptyBorder(5, 5, 5, 5)
---
+++ src/main/scala/org/kleemann/predprey/swing/MapComponent.scala
@@ -0,0 +1,10 @@
+package org.kleemann.predprey.swing
+
+import swing._
+
+class MapComponent extends Component {
+  border = Swing.LineBorder(java.awt.Color.BLACK)
+  // TODO: example code has this without Dimension object "preferredSize = (300,200)"
+  preferredSize = new java.awt.Dimension(300, 200)
+
+}
