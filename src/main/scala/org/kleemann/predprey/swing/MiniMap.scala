7a79cbea5b31e3184754da3394f5aa39c744bdcb
Robert Kleemann <robert@kleemann.org>
2014-11-08 11:33:47 -0800

    adjust layout sizes

---
+++ src/main/scala/org/kleemann/predprey/swing/MiniMap.scala
@@ -0,0 +1,46 @@
+package org.kleemann.predprey.swing
+
+import swing._
+import java.awt.{Color, Graphics2D, Point, geom}
+import org.kleemann.predprey.model._
+
+class MiniMap(var simulation: Simulation) extends Component {
+  
+  border = Swing.LineBorder(Color.BLACK)
+  // TODO: example code has this without Dimension object "preferredSize = (300,200)"
+  preferredSize = new java.awt.Dimension(simulation.width.toInt, simulation.height.toInt)
+
+  // TODO: seems kind of dumb to set this both here and in the constructor
+  def setSimulation(s: Simulation) {
+    this.simulation = s
+    // I believe that since we haven't resized, we should just
+    // repaint() instead of revalidate() or invalidate()
+    repaint
+  }
+  
+  override def paintComponent(g: Graphics2D) = {
+    super.paintComponent(g)
+
+    // draw simulation coords to component coords 1-1
+    
+    g.setColor(new Color(0xa56648)) // brown background
+    g.fillRect(0, 0, simulation.width.toInt, simulation.height.toInt)
+    
+    // current things are 1x1 boxes
+    for(t <- simulation.things) t match {
+      case gr: Grass => {
+        g.setColor(Color.YELLOW)
+        g.fillRect(gr.loc.x.toInt, gr.loc.y.toInt, 1, 1)
+      }
+      case r: Rabbit => {
+        g.setColor(Color.BLUE)
+        g.fillRect(r.loc.x.toInt, r.loc.y.toInt, 1, 1)
+      }
+      case w: Wolf => {
+        g.setColor(Color.RED)
+        g.fillRect(w.loc.x.toInt, w.loc.y.toInt, 2, 2)
+      }
+    }
+  }
+  
+}
